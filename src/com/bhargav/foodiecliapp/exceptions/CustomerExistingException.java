efa7d649449197b0d04ad33d29d4db8f4335df09
BhargavMallik <ballanibhargavmallik@gmail.com>
2024-07-25 11:59:19 +0530

    feat: UPDATE method save in CustomerServiceImpl in Service

--- src/com/bhargav/foodiecliapp/exceptions/CustomerExistingException.java
+++ src/com/bhargav/foodiecliapp/exceptions/CustomerExistingException.java
@@ -1,5 +1,7 @@
 package com.bhargav.foodiecliapp.exceptions;
 
 public class CustomerExistingException extends Exception{
-
+    public CustomerExistingException(String message) {
+        super(message);
+    }
 }
--- src/com/bhargav/foodiecliapp/service/CustomerServiceImpl.java
+++ src/com/bhargav/foodiecliapp/service/CustomerServiceImpl.java
@@ -2,17 +2,29 @@ package com.bhargav.foodiecliapp.service;
 
 import com.bhargav.foodiecliapp.exceptions.CustomerExistingException;
 import com.bhargav.foodiecliapp.model.Customer;
+import com.bhargav.foodiecliapp.repository.CustomerRepository;
 import com.sun.source.tree.BreakTree;
 
+import java.util.Optional;
+
 public class CustomerServiceImpl  implements CustomerService{
+
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public Customer save(Customer customer) throws CustomerExistingException {
 
         //if customer already exists (id, name, emai) throw CustomerExistsException
+        Optional<Customer> customerById = this.customerRepository.findCustomerById(customer.getId());
+        if(customerById.isPresent())
+            throw new CustomerExistingException("Customer Already Exists with this id: "+customer.getId());
 
         //else save customer in repository
 
-        return null;
+        return this.customerRepository.save(customer);
     }
 }
