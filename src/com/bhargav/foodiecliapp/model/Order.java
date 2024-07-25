25008b85cd95cf4bdebdacc5287eee21540a07a5
BhargavMallik <ballanibhargavmallik@gmail.com>
2024-07-25 06:54:40 +0530

    feat: CREATE a template of foodie-cli-app

--- src/com/bhargav/foodiecliapp/model/Order.java
+++ src/com/bhargav/foodiecliapp/model/Order.java
@@ -1,5 +1,8 @@
 package com.bhargav.foodiecliapp.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Order {
     /*
     add the following properties
@@ -20,4 +23,77 @@ public class Order {
     4. Override hashCode() and equals() methods
     5. Override toString() methods
      */
+
+    private String id;
+    private Customer customer;
+    private Restaurant restaurant;
+    private List<String> dishes;
+    private double price;
+
+    public Order() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<String> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<String> dishes) {
+        this.dishes = dishes;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(price, order.price) == 0 && Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(restaurant, order.restaurant) && Objects.equals(dishes, order.dishes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, restaurant, dishes, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", customer=" + customer +
+                ", restaurant=" + restaurant +
+                ", dishes=" + dishes +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/com/bhargav/foodiecliapp/util/CsvReader.java
+++ src/com/bhargav/foodiecliapp/util/CsvReader.java
@@ -1,4 +1,16 @@
 package com.bhargav.foodiecliapp.util;
 
 public class CsvReader {
+
+    public void readDishesFromCsv(){
+
+    }
+
+    public void readCustomersFromCsv(){
+
+    }
+
+    public void readRestaurantsFromCsv(){
+
+    }
 }
