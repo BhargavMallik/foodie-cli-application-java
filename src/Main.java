af6228a70a75568ca77af5956eeeaa4c188e94b9
BhargavMallik <ballanibhargavmallik@gmail.com>
2024-07-25 11:32:56 +0530

    feat: CREATE CustomerRepository class in Repository

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import com.bhargav.foodiecliapp.repository.CustomerRepository;
 import com.bhargav.foodiecliapp.util.CsvReader;
+
+import javax.sound.midi.Soundbank;
 
 public class Main {
 
     public static void main(String[] args) {
-        CsvReader csvReader = new CsvReader();
+
+        /*CsvReader csvReader = new CsvReader();
         System.out.println(csvReader.readCustomersFromCsv());
+         */
+
+        CustomerRepository customerRepository = new CustomerRepository();
+        System.out.println(customerRepository.getAllCustomers());
     }
 }
---
+++ src/com/bhargav/foodiecliapp/repository/CustomerRepository.java
@@ -0,0 +1,26 @@
+package com.bhargav.foodiecliapp.repository;
+
+import com.bhargav.foodiecliapp.model.Customer;
+import com.bhargav.foodiecliapp.util.CsvReader;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerRepository {
+
+    private List<Customer> customerList;
+
+    public CustomerRepository(){
+        CsvReader csvReader = new CsvReader();
+        this.customerList = csvReader.readCustomersFromCsv();
+    }
+
+    public List<Customer> getAllCustomers(){
+        return this.customerList;
+    }
+
+    public Customer save(Customer customer){
+        this.customerList.add(customer);
+        return customer;
+    }
+}
