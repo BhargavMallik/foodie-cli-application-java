8cb2dcb33ef4d5ae62bb81518860fdecadd0a520
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-02 17:10:27 +0530

    feat: Added methods for CustomerRepository

--- src/com/bhargav/foodiecliapp/repository/CustomerRepository.java
+++ src/com/bhargav/foodiecliapp/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.bhargav.foodiecliapp.repository;
 
 import com.bhargav.foodiecliapp.model.Customer;
 import com.bhargav.foodiecliapp.util.CsvReader;
+import com.bhargav.foodiecliapp.util.Factory;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,8 +14,7 @@ public class CustomerRepository
 
     public CustomerRepository()
     {
-        CsvReader csvReader = new CsvReader();
-        this.customerList = csvReader.readCustomersFromCsv();
+        this.customerList = Factory.getCsvReader().readCustomersFromCsv();
     }
 
     public List<Customer> getAllCustomers()
@@ -22,7 +22,7 @@ public class CustomerRepository
         return this.customerList;
     }
 
-    public Customer save(Customer customer)
+    public Customer saveCustomer(Customer customer)
     {
         this.customerList.add(customer);
         return customer;
@@ -31,5 +31,32 @@ public class CustomerRepository
     public Optional<Customer> findCustomerById(String id)
     {
          return this.customerList.stream().filter(customer -> customer.getId().equals(id)).findFirst();
+    }
+
+    public Optional<Customer> findCustomerByEmail(String email)
+    {
+        return this.customerList.stream().filter(customer -> customer.getEmail().equalsIgnoreCase(email)).findFirst();
+    }
+
+    public Optional<Customer> findCustomerByEmailAndPassword(String email, String password)
+    {
+        return this.customerList.stream().filter(customer -> customer.getEmail().equalsIgnoreCase(email) && customer.getPassword().equals(password)).findFirst();
+    }
+
+    public void delete(Customer customer)
+    {
+        this.customerList.remove(customer);
+    }
+
+    public Customer updateCustomer(Customer customerToBeUpdated)
+    {
+        Optional<Customer> updateCustomer = this.customerList.stream().filter(customer -> customer.getId().equals(customerToBeUpdated.getId())).findFirst().map(customer -> {
+            customer.setName(customerToBeUpdated.getName());
+            customer.setEmail(customerToBeUpdated.getEmail());
+            customer.setPassword(customerToBeUpdated.getPassword());
+
+            return customer;
+        });
+        return updateCustomer.orElse(null);
     }
 }
