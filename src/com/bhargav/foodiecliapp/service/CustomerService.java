9f2a48c89882efe35fe3f669d875583ddd022624
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-02 19:03:52 +0530

    feat: Implemented Services for Customer

--- src/com/bhargav/foodiecliapp/service/CustomerService.java
+++ src/com/bhargav/foodiecliapp/service/CustomerService.java
@@ -1,9 +1,26 @@
 package com.bhargav.foodiecliapp.service;
 
 import com.bhargav.foodiecliapp.exceptions.CustomerExistingException;
+import com.bhargav.foodiecliapp.exceptions.CustomerNotExistingException;
 import com.bhargav.foodiecliapp.model.Customer;
+import com.bhargav.foodiecliapp.repository.CustomerRepository;
+
+import java.util.List;
 
 public interface CustomerService {
 
-    public Customer save(Customer customer) throws CustomerExistingException;
+    public Customer saveCustomer(Customer customer) throws CustomerExistingException;
+
+    public void deleteCustomer(String id) throws CustomerNotExistingException;
+
+    public List<Customer> getCustomersList();
+
+    public Customer getCustomerById(String id) throws CustomerNotExistingException;
+
+    public Customer getCustomerByEmail(String email) throws CustomerNotExistingException;
+
+    public Customer updateCustomer(Customer customer) throws CustomerNotExistingException;
+
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotExistingException;
+
 }
--- src/com/bhargav/foodiecliapp/service/CustomerServiceImpl.java
+++ src/com/bhargav/foodiecliapp/service/CustomerServiceImpl.java
@@ -1,10 +1,11 @@
 package com.bhargav.foodiecliapp.service;
 
 import com.bhargav.foodiecliapp.exceptions.CustomerExistingException;
+import com.bhargav.foodiecliapp.exceptions.CustomerNotExistingException;
 import com.bhargav.foodiecliapp.model.Customer;
 import com.bhargav.foodiecliapp.repository.CustomerRepository;
-import com.sun.source.tree.BreakTree;
 
+import java.util.List;
 import java.util.Optional;
 
 public class CustomerServiceImpl  implements CustomerService{
@@ -16,7 +17,7 @@ public class CustomerServiceImpl  implements CustomerService{
     }
 
     @Override
-    public Customer save(Customer customer) throws CustomerExistingException {
+    public Customer saveCustomer(Customer customer) throws CustomerExistingException {
 
         //if customer already exists (id, name, emai) throw CustomerExistsException
         Optional<Customer> customerById = this.customerRepository.findCustomerById(customer.getId());
@@ -25,6 +26,72 @@ public class CustomerServiceImpl  implements CustomerService{
 
         //else save customer in repository
 
-        return this.customerRepository.save(customer);
+        return this.customerRepository.saveCustomer(customer);
     }
+
+    @Override
+    public void deleteCustomer(String id) throws CustomerNotExistingException {
+        Optional<Customer> customerById = this.customerRepository.findCustomerById(id);
+        if(customerById.isEmpty())
+        {
+            throw new CustomerNotExistingException("Customer Not Found");
+        }
+        this.customerRepository.delete(customerById.get());
+    }
+
+    @Override
+    public List<Customer> getCustomersList()
+    {
+        return customerRepository.getAllCustomers();
+    }
+
+    @Override
+    public Customer getCustomerById(String id) throws CustomerNotExistingException
+    {
+         Optional<Customer> customerById = this.customerRepository.findCustomerById(id);
+
+         if(customerById.isEmpty())
+         {
+             throw new CustomerNotExistingException("Customer Not Found in this Id : " + id);
+         }
+         return customerById.get();
+    }
+
+    @Override
+    public Customer getCustomerByEmail(String email) throws CustomerNotExistingException
+    {
+        Optional<Customer> customerByEmail = this.customerRepository.findCustomerByEmail(email);
+
+        if(customerByEmail.isEmpty())
+        {
+            throw new CustomerNotExistingException("Customer Not Found by this Email" + email);
+        }
+        return customerByEmail.get();
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) throws CustomerNotExistingException
+    {
+        Optional<Customer> customerById = this.customerRepository.findCustomerById(customer.getId());
+
+        if(customerById.isEmpty())
+        {
+            throw new CustomerNotExistingException("Customer Not Exists");
+        }
+        return this.customerRepository.updateCustomer(customer);
+    }
+
+    @Override
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotExistingException
+    {
+        Optional<Customer> customerByEmailAndPassword = this.customerRepository.findCustomerByEmailAndPassword(email,password);
+
+        if(customerByEmailAndPassword.isEmpty())
+        {
+            throw new CustomerNotExistingException("Invalid Email or Password");
+        }
+        return customerByEmailAndPassword.get();
+    }
+
+
 }
