91d2eacfce130ca8d582e61074338b9754393b8f
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-02 20:02:10 +0530

    feat: Implement Main class fully

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.bhargav.foodiecliapp.repository.CustomerRepository;
+import com.bhargav.foodiecliapp.ui.Menu;
 import com.bhargav.foodiecliapp.util.CsvReader;
 
 import javax.sound.midi.Soundbank;
@@ -7,11 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readCustomersFromCsv());
-         */
-
-        CustomerRepository customerRepository = new CustomerRepository();
-        System.out.println(customerRepository.getAllCustomers());
+        Menu menu = new Menu();
+        menu.displayMenu();
     }
 }
