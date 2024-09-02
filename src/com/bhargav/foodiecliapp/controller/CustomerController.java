d44f9dc6bf35cec493451d3841f3bae352d0ae9f
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-02 19:28:22 +0530

    feat: Added Controller for Customer

--- src/com/bhargav/foodiecliapp/controller/CustomerController.java
+++ src/com/bhargav/foodiecliapp/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.bhargav.foodiecliapp.controller;
 
 import com.bhargav.foodiecliapp.exceptions.CustomerExistingException;
+import com.bhargav.foodiecliapp.exceptions.CustomerNotExistingException;
 import com.bhargav.foodiecliapp.model.Customer;
 import com.bhargav.foodiecliapp.service.CustomerServiceImpl;
+
+import java.util.List;
 
 public class CustomerController
 {
@@ -13,8 +16,45 @@ public class CustomerController
         this.customerServiceImpl = customerServiceImpl;
     }
 
-    public Customer save(Customer customer) throws CustomerExistingException
+    public Customer saveCustomer(Customer customer) throws CustomerExistingException
     {
-        return this.customerServiceImpl.save(customer);
+        return this.customerServiceImpl.saveCustomer(customer);
     }
+
+    public void deleteCustomer(String id) throws CustomerNotExistingException
+    {
+        this.customerServiceImpl.deleteCustomer(id);
+    }
+
+    public List<Customer> getCustomersList()
+    {
+        return this.customerServiceImpl.getCustomersList();
+    }
+
+    public Customer getCustomerById(String id) throws CustomerNotExistingException
+    {
+        return this.customerServiceImpl.getCustomerById(id);
+    }
+
+    public Customer getCustomerByEmail(String email) throws CustomerNotExistingException
+    {
+        return this.customerServiceImpl.getCustomerByEmail(email);
+    }
+
+    public Customer getCurrentLoggedInCustomer()
+    {
+        return this.customerServiceImpl.currentLoggedInCustomer();
+    }
+
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotExistingException
+    {
+        Customer customer = this.customerServiceImpl.validateCustomerLogin(email,password);
+
+        if(customer != null)
+        {
+            this.customerServiceImpl.setCurrentLoggedInCustomer(customer);
+        }
+        return customer;
+    }
+
 }
