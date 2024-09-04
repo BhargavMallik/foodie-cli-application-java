59319d7164fed51f10a5b8408e196d2144f9c15b
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-04 14:04:31 +0530

    feat: Implemented Controller for Restaurant

---
+++ src/com/bhargav/foodiecliapp/controller/RestaurantController.java
@@ -0,0 +1,45 @@
+package com.bhargav.foodiecliapp.controller;
+
+import com.bhargav.foodiecliapp.exceptions.RestaurantExistsException;
+import com.bhargav.foodiecliapp.exceptions.RestaurantNotExistsException;
+import com.bhargav.foodiecliapp.model.Restaurant;
+import com.bhargav.foodiecliapp.service.RestaurantServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RestaurantController
+{
+    private final RestaurantServiceImpl restaurantService;
+
+    public RestaurantController(RestaurantServiceImpl restaurantService)
+    {
+        this.restaurantService = restaurantService;
+    }
+
+    public List<Restaurant> getAllRestaurants()
+    {
+        return this.restaurantService.getAllRestaurants();
+    }
+
+    public Restaurant saveRestaurant(Restaurant restaurant) throws RestaurantExistsException
+    {
+        return this.restaurantService.saveRestaurant(restaurant);
+    }
+
+    public Optional<Restaurant> findRestaurantById(String id) throws RestaurantNotExistsException
+    {
+        return this.restaurantService.findRestaurantById(id);
+    }
+
+    public Restaurant updateRestaurant(Restaurant restaurantToBeUpdated) throws RestaurantNotExistsException
+    {
+        return this.restaurantService.updateRestaurant(restaurantToBeUpdated);
+    }
+
+    public void deleteRestaurant(Restaurant restaurant) throws RestaurantNotExistsException
+    {
+        this.restaurantService.deleteRestaurant(restaurant);
+    }
+
+}
