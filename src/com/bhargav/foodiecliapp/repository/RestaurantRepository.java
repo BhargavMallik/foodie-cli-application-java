4a135db8506f49d1b3331c0052953e47db416fef
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-04 13:23:01 +0530

    feat: Implemented Repository for Restaurant

--- src/com/bhargav/foodiecliapp/repository/RestaurantRepository.java
+++ src/com/bhargav/foodiecliapp/repository/RestaurantRepository.java
@@ -1,6 +1,8 @@
 package com.bhargav.foodiecliapp.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.bhargav.foodiecliapp.model.Restaurant;
 import com.bhargav.foodiecliapp.Factory;
 
@@ -18,11 +20,33 @@ public class RestaurantRepository
         return this.restaurantList;
     }
 
-    public Restaurant save(Restaurant restaurant)
+    public Restaurant saveRestaurant(Restaurant restaurant)
     {
         this.restaurantList.add(restaurant);
         return restaurant;
     }
 
+    public Optional<Restaurant> findRestaurantById(String id)
+    {
+        return this.restaurantList.stream().filter(restaurant -> restaurant.getId().equals(id)).findFirst();
+    }
 
+    public Restaurant updateRestaurant(Restaurant restaurantToBeUpdated)
+    {
+        Optional<Restaurant> restaurantOptional = this.restaurantList.stream().filter(restaurant -> restaurant.getId().equals(restaurantToBeUpdated.getId()))
+                .findFirst()
+                .map(restaurant ->{
+                    restaurant.setName(restaurantToBeUpdated.getName());
+                    restaurant.setMenu(restaurantToBeUpdated.getMenu());
+                    restaurant.setAddress(restaurantToBeUpdated.getAddress());
+                    return restaurant;
+                        }
+                );
+        return restaurantOptional.orElse(null);
+    }
+
+    public void deleteRestaurant(Restaurant restaurant)
+    {
+        this.restaurantList.remove(restaurant);
+    }
 }
