7b7a07c173a373c11934a287a1cd5b80af49e448
BhargavMallik <bhargavmallikballani@gmail.com>
2024-09-04 14:09:34 +0530

    feat: Implemented Factory for Restaurant

--- src/com/bhargav/foodiecliapp/Factory.java
+++ src/com/bhargav/foodiecliapp/Factory.java
@@ -1,8 +1,11 @@
 package com.bhargav.foodiecliapp;
 
 import com.bhargav.foodiecliapp.controller.CustomerController;
+import com.bhargav.foodiecliapp.controller.RestaurantController;
 import com.bhargav.foodiecliapp.repository.CustomerRepository;
+import com.bhargav.foodiecliapp.repository.RestaurantRepository;
 import com.bhargav.foodiecliapp.service.CustomerServiceImpl;
+import com.bhargav.foodiecliapp.service.RestaurantServiceImpl;
 import com.bhargav.foodiecliapp.util.CsvReader;
 
 public class Factory
@@ -11,16 +14,34 @@ public class Factory
     {
         return Holder.CSV_READER;
     }
-    public static CustomerRepository getCustomerRepository() {
+    public static CustomerRepository getCustomerRepository()
+    {
         return Holder.CUSTOMER_REPOSITORY;
     }
 
-    public static CustomerServiceImpl getCustomerService() {
+    public static CustomerServiceImpl getCustomerService()
+    {
         return Holder.CUSTOMER_SERVICE;
     }
 
-    public static CustomerController getCustomerController() {
+    public static CustomerController getCustomerController()
+    {
         return Holder.CUSTOMER_CONTROLLER;
+    }
+
+    public static RestaurantRepository getRestaurantRepository()
+    {
+        return Holder.RESTAURANT_REPOSITORY;
+    }
+
+    public static RestaurantServiceImpl getRestaurantService()
+    {
+        return Holder.RESTAURANT_SERVICE;
+    }
+
+    public static RestaurantController getRestaurantController()
+    {
+        return Holder.RESTAURANT_CONTROLLER;
     }
 
     private static class Holder
@@ -30,5 +51,10 @@ public class Factory
         private static final CustomerRepository CUSTOMER_REPOSITORY = new CustomerRepository();
         private static final CustomerServiceImpl CUSTOMER_SERVICE = new CustomerServiceImpl(CUSTOMER_REPOSITORY);
         private static final CustomerController CUSTOMER_CONTROLLER = new CustomerController(CUSTOMER_SERVICE);
+
+        private static final RestaurantRepository RESTAURANT_REPOSITORY = new RestaurantRepository();
+        private static final RestaurantServiceImpl RESTAURANT_SERVICE = new RestaurantServiceImpl(RESTAURANT_REPOSITORY);
+        private static final RestaurantController RESTAURANT_CONTROLLER = new RestaurantController(RESTAURANT_SERVICE);;
+
     }
 }
